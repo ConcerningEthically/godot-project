247696d3d1317c1850c09402834295f18acf9810
dchen <danielc204@deltalearns.ca>
2025-02-05 10:23:43 -0800

    remake of basically everything

---
+++ Mob.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+# Minimum speed of the mob (m/s)
+@export var min_speed = 10
+# Maximum speed of the mob (m/s)
+@export var max_speed = 18
+
+
+func _physics_process(_delta):
+	move_and_slide()
+
+func intitialize(start_position, player_position):
+	# Position mob at start position
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# Rotates the mob in a random direction so it does not face the player directly
+	rotate_y(randf_range(-PI / 4, PI / 4))
+
+	# We calculate a random speed (integer)
+	var random_speed = randi_range(min_speed, max_speed)
+	# Calculate a forward velocity that represents the speed.
+	velocity = Vector3.FORWARD * random_speed
+	# Rotate the velocity vector based on the mob's Y rotation
+	velocity = velocity.rotated(Vector3.UP, rotation.y) 
+	
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+	
---
+++ main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var mob_scene:PackedScene
+
+func _on_mob_timer_timeout():
+	# Create a mob scene instance
+	var mob = mob_scene.instantiate()
+	
+	# Choose a random position on the spawn path 
+	var mob_spawn_location = get_node("Spawnpath/Spawnlocation")
+	# Give the mob a random offset(progress across the screen)
+	mob_spawn_location.progress_ratio = randf()
+	
+	# Get player position 
+	var player_position = $Player.position 
+	# Spawn mob at random spawn location, player position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	#spawn the mob
+	add_child(mob)
